de636bbba9c7f177bd43a71fa7c2635e275a9e32
Meeraj29 <meerajmathin@gmail.com>
2024-09-19 09:26:20 +0530

    Add files via upload
    completed the website

---
+++ basketball.py
@@ -0,0 +1,18 @@
+inp1=int(input())
+inp2=int(input())
+arr=list(map(int,input().split()))
+mx=-1
+for i in range(0,len(arr)-inp2+1):
+    temp=arr[i:i+inp2]
+    k,s=1,0
+    for j in temp:
+        s+=(j*k)#p=k*j
+        k+=1#s+=p
+    if s>mx:
+        mx=s
+print(mx)
+
+5
+2
+1 2 3 4 5
+14
